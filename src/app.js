c3adee34dd5ffd8664976b030f1ce3a0380e956a
rajan <rajangreedge@gmail.com>
2019-03-08 13:54:22 +0530

    created help and about html page inside public folder

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ console.log(path.join(__dirname,"../public"));
 
 const app = express();//takes no argument.
 const publicDirectory = path.join(__dirname, "../public");
+
 app.use(express.static(publicDirectory));
 
 
-app.get('/help',(req, res)=>{///help
+
+
+/* app.get('/help',(req, res)=>{///help
     //array of obj or obj as a reponse.
     res.send([{
         name:'Andrew',
@@ -20,7 +23,7 @@ app.get('/help',(req, res)=>{///help
 
 app.get('/about',(req,res)=>{///about
     res.send("<h1>About</h1>");
-});
+}); */
 
 app.get('/weather',(req,res)=>{///weather
     res.send({
