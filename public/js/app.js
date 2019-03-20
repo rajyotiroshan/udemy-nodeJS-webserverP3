28e2736d5f25e1bf93c13bf2b290c60e3df1073a
rajan <rajangreedge@gmail.com>
2019-03-20 21:44:50 +0530

    fetch in pulic app.js folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log("client side js file");
+
+fetch('http://localhost:3000/weather?address=!').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error);
+        }else {
+           console.log(data); 
+        }
+        
+    });
+})
