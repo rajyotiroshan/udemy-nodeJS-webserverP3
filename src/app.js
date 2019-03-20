5aec9cfbf9e27aaefa573b5885484a59f6546f85
rajan <rajangreedge@gmail.com>
2019-03-20 21:20:27 +0530

    destructuring error fixed

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ app.get('/weather',(req,res)=>{///weather
     }
     
     //request for forecast data.
-    geocode(req.query.address, (error,{latitude, longitude, location})=>{
+    geocode(req.query.address, (error,{latitude, longitude, location}= {})=>{
         if(error){
             return res.send({
                 error
