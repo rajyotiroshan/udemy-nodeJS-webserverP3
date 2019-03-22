ccb69cfeca888be09249418a6f65c44255480f27
rajan <rajangreedge@gmail.com>
2019-03-22 07:53:42 +0530

    display forecasy msg on the page

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,24 @@ console.log("client side js file");
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const msg1 = document.querySelector('#msg-1');
+const msg2 = document.querySelector('#msg-2');
+
+
 //listener for dearch form submit evt
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault();    
     const location = search.value;
-    
+    msg1.textContent = 'Loading...';
+    msg2.textContent = '';
     //fetch address.
     fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
-                console.log(data.error);
+               msg1.textContent = data.error;
             }else {
-            console.log(data); 
+                msg1.textContent= data.location;
+                msg2.textContent = data.forecast;
             }
         });
     })
