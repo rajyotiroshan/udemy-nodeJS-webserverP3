e91a15ede427300ac6d26051eccd40435b9de98f
rajan <rajangreedge@gmail.com>
2019-03-22 07:24:37 +0530

    search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
 console.log("client side js file");
 
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error);
-        }else {
-           console.log(data); 
-        }
-        
-    });
-})
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+//listener for dearch form submit evt
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault();    
+    const location = search.value;
+    
+    //fetch address.
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error);
+            }else {
+            console.log(data); 
+            }
+        });
+    })
+    console.log(location);
+});
